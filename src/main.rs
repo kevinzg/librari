8dbfcfd3180d8ea703fdbde4cbfe01453ac2aca5
Kevin Zúñiga <kevin.zun@gmail.com>
2024-02-08 19:09:32 -0500

    feat: print epub titles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::env;
+use std::path::Path;
+
+use epub::doc::EpubDoc;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: {} <directory>", args[0]);
+        return;
+    }
+
+    let dir = &args[1];
+
+    for entry in walkdir::WalkDir::new(dir)
+        .max_depth(3) // enough to run on Calibre's library
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_type().is_file() && e.path().extension().map_or(false, |e| e == "epub"))
+    {
+        if let Ok(title) = read_epub(entry.path()) {
+            println!("{}", title);
+        } else {
+            println!("Error reading {}", entry.path().display());
+        }
+    }
+}
+
+fn read_epub(path: &Path) -> Result<String, String> {
+    let doc = EpubDoc::new(path).map_err(|e| e.to_string())?;
+    if let Some(title) = doc.mdata("title") {
+        return Ok(title);
+    } else {
+        return Err("No title found".to_string());
+    }
 }
