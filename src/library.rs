d4bdcb1fc90eaac9ed32bdc51993284467974d8c
Kevin Zúñiga <kevin.zun@gmail.com>
2024-02-28 20:25:29 -0500

    feat: use arc mutex instead of rc refcell

--- src/library.rs
+++ src/library.rs
@@ -1,30 +1,36 @@
 #![allow(dead_code)]
 #![allow(unused)]
 
+use epub::doc::{EpubDoc, NavPoint};
 use std::{
     cell::RefCell,
     fs::File,
-    io::BufReader,
+    io::{BufReader, Read},
     num::NonZeroUsize,
     path::{Path, PathBuf},
     rc::Rc,
+    sync::{Arc, Mutex},
 };
+
+use crate::utils;
 
 // TODO: Should I use tokio::{BufReader, File} instead?
 type Epub = epub::doc::EpubDoc<BufReader<File>>;
 
+// TODO: Can I use RwLock instead of Mutex?
+// TODO: Should I use tokio's Mutex?
 pub struct Library {
     base_path: PathBuf,
-    db: rusqlite::Connection,
-    cache: RefCell<lru::LruCache<usize, Rc<RefCell<Epub>>>>,
+    db: Mutex<rusqlite::Connection>,
+    cache: Mutex<lru::LruCache<usize, Arc<Mutex<Epub>>>>,
 }
 
 impl Library {
     pub fn new(path: &Path, db: rusqlite::Connection) -> Self {
         Self {
             base_path: path.to_owned(),
-            db,
-            cache: RefCell::new(lru::LruCache::new(NonZeroUsize::new(5).unwrap())),
+            db: Mutex::new(db),
+            cache: Mutex::new(lru::LruCache::new(NonZeroUsize::new(5).unwrap())),
         }
     }
 
@@ -41,7 +47,7 @@ impl Library {
     ) -> Result<(String, Vec<u8>), LibraryError> {
         let info = self.get_book_info(slug)?;
         let binding = self.get_epub_doc(&info)?;
-        let mut doc = binding.borrow_mut();
+        let mut doc = binding.lock().unwrap();
         let content = doc
             // TODO: Make this method not need a mutable reference to self
             .get_resource_by_path(&res_path)
@@ -57,6 +63,8 @@ impl Library {
         let id = get_id(slug)?;
         let info = self
             .db
+            .lock()
+            .unwrap()
             .query_row("SELECT title, path FROM books WHERE id = ?", [id], |row| {
                 let title: String = row.get(0)?;
                 let path: String = row.get(1)?;
@@ -71,11 +79,11 @@ impl Library {
     }
 
     /// Get the epub document from the cache or load it from the file system
-    fn get_epub_doc(&self, info: &BookInfo) -> Result<Rc<RefCell<Epub>>, LibraryError> {
-        let cache = &mut self.cache.borrow_mut();
+    fn get_epub_doc(&self, info: &BookInfo) -> Result<Arc<Mutex<Epub>>, LibraryError> {
+        let cache = &mut self.cache.lock().unwrap();
         let ptr = cache.try_get_or_insert(info.id, || {
             let doc = self.load_epub_doc(&info.path)?;
-            Ok(Rc::new(RefCell::new(doc)))
+            Ok(Arc::new(Mutex::new(doc)))
         })?;
         Ok(ptr.clone())
     }
